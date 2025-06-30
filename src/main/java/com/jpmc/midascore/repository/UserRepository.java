6eeaecd5b95b6431785d6715b72c9dfaa191740b
AjayKumbham <ajaygoud.kumbham@gmail.com>
2025-06-30 20:38:50 +0530

    fix: h2 integration

--- src/main/java/com/jpmc/midascore/repository/UserRepository.java
+++ src/main/java/com/jpmc/midascore/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.jpmc.midascore.entity.UserRecord;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserRecord, Long> {
+    UserRecord findByName(String name);
 }
